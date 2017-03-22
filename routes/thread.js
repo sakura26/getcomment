888dca0ea633e88097ce85fb40ddaf9b0f653685
sakura26 <sakura26@ccsakura-net.com>
2017-03-22 20:35:28 +0800

    I can promise! object operation is ok

--- routes/thread.js
+++ routes/thread.js
@@ -14,19 +14,17 @@ var ThreadSchema = mongoose.Schema({
     updated_at : { type: Date }
 });
 var CommentSchema = new Schema({
+	threadId : String,
     nickname    : String,
     response    : String,
     rank    : String,
+    email	: String, 
     public	: String, 
     updated_at : Date
 });
-/*ThreadSchema.methods.getThread = function(id, callback) {
-  this.findOne({ email: "qqq" }, function (a,b){loglog("a:"+a+" b:"+b);})
-//this.findOne({ email: ObjectId(sanitize(id)) }, callback);
-  .exec(callback);
-}*/
 var Thread = mongoose.model( 'Thread', ThreadSchema );
-//mongoose.model( 'Comment', Comment );
+var Comment = mongoose.model( 'Comment', CommentSchema );
+
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -56,8 +54,13 @@ router.get('/:id', function(req, res) {
 	    res.end(JSON.stringify({status:"error", msg:"no such thread"}));
 	    return;
 	  }
-	  res.writeHead( 201, {'Content-Type' : 'text/plain'});
-	  res.end(JSON.stringify(data));
+	  Comment.find({threadId:req.params.id}, function (err, comments) {
+	  	var qq = data.toObject();
+	  	qq.comments = comments;
+		res.writeHead( 201, {'Content-Type' : 'text/plain'});
+		res.end(JSON.stringify(qq));
+		//res.end(JSON.stringify(comments));
+	  })
 	})
 });
 
@@ -72,6 +75,17 @@ router.post('/', function(req, res) {
 	});
 	loglog("addThread "+thisthread._id,"INFO");
 	return thisthread._id;
+});
+router.post('/addThread', function(req, res) {
+	//TODO: validate email!
+	//TODO: send notify email!
+	var thisthread = new Thread( {title: req.body.title, ownerSaid: req.body.ownerSaid, tags: req.body.tags, email: req.body.email, defaultResponse: req.body.defaultResponse, public: req.body.public} );//mongoose.model('Thread');
+	thisthread.save(function (err, fluffy) {
+	  if (err) return console.error(err);
+	  res.redirect("/thread/"+thisthread._id+"/show");
+	});
+	loglog("addThread "+thisthread._id,"INFO");
+	return;
 });
 
 router.get('/:id/show', function(req, res) {
@@ -88,9 +102,26 @@ router.get('/:id/show', function(req, res) {
 	    res.end(JSON.stringify({status:"error", msg:"no such thread"}));
 	    return;
 	  }
-	  res.render('threadshow', data);
-	})
-});router.get('/:id/edit', function(req, res) {
+	  //get comments if needed
+	  loglog("data.public:"+data.public,"DEBUG");
+	  var qq = data.toObject();
+	  // I know how to use promise!!! (crying)
+	  var loaded = new Promise(function(resolve, reject) {
+		  if (qq.public!="yes"){
+		  	resolve();
+		  }
+		  Comment.find({threadId:req.params.id}, function (err, comments) {
+		  	qq.comments = comments;
+	  		resolve();
+		  })
+	  });
+	  loaded.then(function() {
+	    res.render('threadshow', qq);
+	  });
+	});
+});
+
+router.get('/:id/edit', function(req, res) {
 	//TOOD: validate owner!
 	var thisthread = new Thread();//mongoose.model('Thread');
 	Thread.findOne({_id:ObjectId(req.params.id)}, function (err, data) {
@@ -163,6 +194,36 @@ router.delete('/:id', function(req, res) {
 	  data.remove();
 	  res.writeHead( 201, {'Content-Type' : 'text/plain'});
 	  res.end(JSON.stringify({status:"success", msg:"obj removed"}));
+	})
+});
+
+router.post('/:id/comment', function(req, res) {
+
+	//TODO: validate email!
+	//TODO: send notify email!
+	Thread.findOne({_id:ObjectId(req.params.id)}, function (err, data) {
+	  if (err){
+	  	res.writeHead( 500, {'Content-Type' : 'text/plain'});
+	    res.end(JSON.stringify({status:"error", msg:"exception:"+err}));
+	    return;
+	  } 
+	  if (nov(data)){
+		res.writeHead( 404, {'Content-Type' : 'text/plain'});
+	    res.end(JSON.stringify({status:"error", msg:"no such thread"}));
+	    return;
+	  }
+	  	var thiscomment = new Comment( {threadId: req.params.id, nickname: req.body.nickname, response: req.body.response, rank: req.body.rank, email: req.body.email, public: req.body.public} );//mongoose.model('Thread');
+		thiscomment.save(function (err, fluffy) {
+		    if (err){
+			  	//res.render('commentFail', data);
+			  	loglog("Faile to add comment: "+err,"ERROR");
+				res.end("ERROR: Faile to add comment: "+err);
+				//res.render('commentFail', data);
+			  	return;
+		    } 
+			loglog("addComment "+thiscomment._id,"INFO");
+			res.render('commentSuccess', data);
+		});
 	})
 });
 module.exports = router;
